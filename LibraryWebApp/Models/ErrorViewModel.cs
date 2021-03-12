16354c0881c688c9a15ebcb86af610ba540f3c96
Dariel <dariel901231@gmail.com>
2021-03-11 19:48:10 -0500

    Add project files.

---
+++ LibraryWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibraryProjectApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
